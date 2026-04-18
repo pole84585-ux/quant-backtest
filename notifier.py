5bf401b372d00488b74f3ebd0ddf9690cec84282
pole84585-ux <pole84585@gmail.com>
2026-04-18 14:45:53 +0800

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,20 +1,48 @@
 import requests
 from config import BOT_TOKEN, CHAT_ID
+from cache import load_cache, save_cache
+
+
+cache = load_cache()
 
 
 def send(msg):
 
     if not BOT_TOKEN or not CHAT_ID:
-        print("未配置TG，输出如下：")
         print(msg)
+        return
+
+    key = str(hash(msg))
+
+    if key in cache:
+        print("重复信号跳过")
         return
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    try:
-        requests.post(url, data={
-            "chat_id": CHAT_ID,
-            "text": msg
-        })
-    except Exception as e:
-        print("TG发送失败:", e)
+    success = False
+
+    for i in range(3):
+        try:
+            r = requests.post(
+                url,
+                data={
+                    "chat_id": CHAT_ID,
+                    "text": msg
+                },
+                timeout=10
+            )
+
+            if r.status_code == 200:
+                success = True
+                break
+
+        except Exception as e:
+            print(f"TG失败{i+1}:", e)
+
+    if success:
+        cache.add(key)
+        save_cache(cache)
+        print("发送成功")
+    else:
+        print("发送失败:", msg)
